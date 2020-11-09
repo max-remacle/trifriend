31beb5ef9775b1814938196cc6c7872d4766e769
Your Namemax-remacle <remacle.maxime@gmail.com>
2020-11-10 12:32:20 +1300

    added persistance to reduc store

--- client/index.js
+++ client/index.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { HashRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { HashRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './store'
-import App from './components/App'
+import { store, persistor } from "./store";
+import App from "./components/App";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
     <Provider store={store}>
       <Router>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Router>
     </Provider>,
-    document.getElementById('app')
-  )
-})
+    document.getElementById("app")
+  );
+});
--- client/store.js
+++ client/store.js
@@ -1,9 +1,12 @@
 import { createStore } from 'redux'
+import { persistStore } from 'redux-persist'
 
 import reducers from './reducers'
 
-const store = createStore(reducers,
+export const store = createStore(reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-export default store
+export const persistor = persistStore(store)
+
+export default { store, persistor }
